b5c5c842d15ffa28ec306937effa16c0caa290fd
Jamesmed1999 <Jamesmed1999@gmail.com>
2021-01-21 23:28:48 -0500

    almost done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { MONGODB } = require('./config1.js');
 
 
 const pubsub = new PubSub();
-
+const PORT = process.env.port || 5000;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -19,7 +19,7 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true,useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
